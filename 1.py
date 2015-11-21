0ddc588857c3e373e373c6b076aa41b76ad80dda
aamerino <amerinotsutsui@gmail.com>
2015-11-21 16:08:49 +0100

    Pep8 ej1

--- 1.py
+++ 1.py
@@ -8,6 +8,6 @@ numeroTriple = numero * 3
 
 print("Numero introducido " + str(numero))
 
-print("Doble " +  str(numeroDoble))
+print("Doble " + str(numeroDoble))
 
 print("Triple " + str(numeroTriple))
